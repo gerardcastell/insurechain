0f0d21a73c174e2c83085191767ad5a525a10566
Gerard Castell <gerardcastell97@gmail.com>
2022-10-22 19:14:08 +0200

    feat: add husky

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,10 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  constructor() {}
-
-  async getData() {
-    return { hola: 'welcome' };
-  }
-}
+export class AppService {}
