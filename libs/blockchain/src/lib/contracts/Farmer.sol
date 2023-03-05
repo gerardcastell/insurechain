cc474851f5ccac85e113cfd1f02826a62561bd05
Gerard Castell <gerardcastell97@gmail.com>
2023-03-05 21:17:05 +0100

    feat(blockchain): add farmer and policy smart contracts v1

---
+++ libs/blockchain/src/lib/contracts/Farmer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./Policy_v2.sol";
+
+
+contract InsuranceFarmer {
+    address insuranceAddress;
+    uint256 minimumBudget = 0.999 ether;
+    mapping(uint256 => address[]) policiesMapping;
+
+    modifier onlyCompany {
+        require(msg.sender == insuranceAddress);
+        _;
+    }
+
+
+   constructor() payable {
+        require(msg.value >= minimumBudget, "Minimum budget is not achieved to start an Insurance Smart Contract" );
+        insuranceAddress = msg.sender;
+    }
+
+    function addBuget() onlyCompany public payable returns (uint256){
+        return address(this).balance;
+    }
+
+    function createPolicy(uint policyHolder, string memory riskObject) public payable returns (address){
+        require(msg.value > 0, "To create a policy you must pay a premium.");
+        Policy policyContract = new Policy(policyHolder, riskObject, msg.value, msg.sender);
+        address policyAddress = address(policyContract);
+        uint holderId = policyHolder;
+        policiesMapping[holderId].push(policyAddress);
+        return address(policyContract);
+    }
+
+    function getUserPolicies(uint userId) public view returns (address[] memory){
+        address[] memory addresses = policiesMapping[userId];
+        return addresses;
+    }
+
+
+}
---
+++ libs/blockchain/src/lib/contracts/Policy_v2.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+struct Claim {
+    string title;
+    address expertAddress;
+    uint incidentDate;
+    uint expenses;
+    bool approved;
+    uint resolveDate;
+}
+
+
+contract Policy {
+    uint policyHolder;
+    string riskObject;
+    uint256 premium;
+    address owner;
+    address insuranceAddress;
+    uint endDate;
+    uint startDate;
+    mapping(uint256 => Claim[]) claims;
+
+
+    modifier onlyCompanyOrOwner(){
+        require(msg.sender == insuranceAddress || msg.sender == owner);
+        _;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyCompany(){
+        require(msg.sender == insuranceAddress);
+        _;
+    }
+
+
+    constructor(uint _policyHolder, string memory _riskObject, uint256 _premium, address _owner){
+        policyHolder = _policyHolder;
+        riskObject = _riskObject;
+        premium = _premium;
+        owner = _owner;
+        insuranceAddress = msg.sender;
+        startDate = block.timestamp;
+    }
+
+    function cancelPolicy() onlyCompanyOrOwner external {
+        endDate = block.timestamp;
+    }
+
+    function makeClaim(uint256 claimId, Claim memory _claim) onlyOwner external {
+        Claim memory newClaim = _claim;
+        claims[claimId].push(newClaim);
+    }
+
+    // Functions for experts
+    function approveClaim(uint256 claimId , uint256 claimExpenses) onlyCompany external {
+        Claim[] storage _claims = claims[claimId];
+        Claim storage claim = _claims[claimId];
+
+        claim.expenses = claimExpenses;
+        resolveClaim(claimId, true);
+    }
+
+    function declineClaim(uint256 claimId) onlyCompany external{
+        resolveClaim(claimId, false);
+    }
+
+    function resolveClaim(uint256 claimId, bool isApproved) internal {
+        Claim[] storage _claims = claims[claimId];
+        Claim storage claim = _claims[claimId];
+
+        claim.resolveDate = block.timestamp;
+        claim.approved = isApproved;
+    }
+}
