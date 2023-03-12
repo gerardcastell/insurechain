a3e6e147c687439ef60db3fc6af994a1c205cba6
Gerard Castell <gerardcastell97@gmail.com>
2023-03-12 16:32:32 +0100

    feat: add unit tests policy smart contract

--- libs/blockchain/src/lib/contracts/Policy_v2.sol
+++ libs/blockchain/src/lib/contracts/Policy_v2.sol
@@ -9,6 +9,7 @@ struct Claim {
     uint expenses;
     bool approved;
     uint resolveDate;
+    bool isResolved;
 }
 
 
@@ -19,26 +20,26 @@ contract Policy { uint policyHolder;
     address insuranceAddress;
     uint endDate;
     uint startDate;
-    mapping(uint256 => Claim[]) claims;
+    mapping(uint256 => Claim) claims;
 
 
     modifier onlyCompanyOrOwner(){
-        require(msg.sender == insuranceAddress || msg.sender == owner);
+        require(msg.sender == insuranceAddress || msg.sender == owner, "Just the policyholder or the insurance company can perform this action");
         _;
     }
 
     modifier onlyOwner(){
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Just the policyholder can perform this action");
         _;
     }
 
     modifier onlyCompany(){
-        require(msg.sender == insuranceAddress);
+        require(msg.sender == insuranceAddress, "Just the insurance company can perform this action");
         _;
     }
 
     modifier isActive(){
-        require(endDate > block.timestamp);
+        require(endDate > block.timestamp, "Policy is not active");
         _;
     }
 
@@ -55,22 +56,22 @@ contract Policy { uint policyHolder;
         endDate = _endDate;
     }
 
-    function cancelPolicy() onlyCompanyOrOwner external {
+    function cancelPolicy() onlyCompanyOrOwner isActive external {
         endDate = block.timestamp;
     }
 
     function makeClaim(uint256 claimId, Claim memory _claim) onlyOwner isActive external {
         Claim memory newClaim = _claim;
-        claims[claimId].push(newClaim);
+        claims[claimId] = newClaim;
+        claims[claimId].isResolved = false;
     }
 
-    // Functions for experts
-    function approveClaim(uint256 claimId , uint256 claimExpenses) onlyCompany external {
-        Claim[] storage _claims = claims[claimId];
-        Claim storage claim = _claims[claimId];
 
-        claim.expenses = claimExpenses;
+    function approveClaim(uint256 claimId , uint256 claimExpenses) onlyCompany external {
+        Claim storage claim = claims[claimId];
+
         resolveClaim(claimId, true);
+        claim.expenses = claimExpenses;
     }
 
     function declineClaim(uint256 claimId) onlyCompany external{
@@ -78,11 +79,13 @@ contract Policy { uint policyHolder;
     }
 
     function resolveClaim(uint256 claimId, bool isApproved) internal {
-        Claim[] storage _claims = claims[claimId];
-        Claim storage claim = _claims[claimId];
+        Claim storage claim = claims[claimId];
+        require(claim.isResolved == false, "Claim is already resolved");
 
         claim.resolveDate = block.timestamp;
+        claims[claimId].isResolved = true;
         claim.approved = isApproved;
+
     }
 
     function getOwnerAddress() onlyCompanyOrOwner external view returns (address) {
