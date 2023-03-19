1b8343a1bfc3439df2e6e33c3c825e92b67e8f28
Gerard Castell <gerardcastell97@gmail.com>
2023-03-19 23:51:16 +0100

    feat: create client web app + added material ui

---
+++ apps/client-web/pages/_app.tsx
@@ -0,0 +1,36 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import './styles.css';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function CustomApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Welcome to client-web!</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default CustomApp;
