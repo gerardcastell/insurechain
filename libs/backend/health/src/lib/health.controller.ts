d61ae42d6f35dd0e59623a8234484c60087af6a5
Gerard Castell <gerardcastell97@gmail.com>
2023-04-07 21:26:53 +0200

    feat: health checks memory and storage

--- libs/backend/health/src/lib/health.controller.ts
+++ libs/backend/health/src/lib/health.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+import {
+  HealthCheckService,
+  HealthCheck,
+  MemoryHealthIndicator,
+  DiskHealthIndicator,
+} from '@nestjs/terminus';
 import { PrismaHealthIndicator } from '@insurechain/backend/prisma';
 
 @Controller('health')
 export class HealthController {
   constructor(
-    private health: HealthCheckService,
-    private prisma: PrismaHealthIndicator
+    private readonly health: HealthCheckService,
+    private readonly prisma: PrismaHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+    private readonly disk: DiskHealthIndicator
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([() => this.prisma.isHealthy('User')]);
+    return this.health.check([
+      () => this.prisma.isHealthy('database'),
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () =>
+        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+    ]);
   }
 }
