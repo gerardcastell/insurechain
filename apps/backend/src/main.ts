2dec976f9b7d7346fe3c73dadae612975e3834f3
Gerard Castell <gerardcastell97@gmail.com>
2023-04-09 15:57:55 +0200

    feat: add cors to backend server

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(globalPrefix, app, document);
-
+  app.enableCors();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
