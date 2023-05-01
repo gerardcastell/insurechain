b69ecd5627755388a0b2e5b37ea25e8a1181e610
Gerard Castell <gerardcastell97@gmail.com>
2023-05-01 22:46:33 +0200

    feat: validate nested quote

--- libs/backend/proposals/src/lib/dtos/Quote.dto.ts
+++ libs/backend/proposals/src/lib/dtos/Quote.dto.ts
@@ -1,15 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsEnum, IsObject, IsOptional } from 'class-validator';
+import {
+  IsArray,
+  IsDefined,
+  IsEnum,
+  IsObject,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
 import { RiskObjectDto } from './RiskObject.dto';
 import { RiskSubjectDto } from './RiskSubject.dto';
 import { Coverage } from '@prisma/client';
+import { Type } from 'class-transformer';
 export class QuoteDto {
   @ApiProperty({ type: () => RiskObjectDto })
+  @IsDefined()
   @IsObject()
+  @ValidateNested()
+  @Type(() => RiskObjectDto)
   riskObject: RiskObjectDto;
 
   @ApiProperty({ type: () => RiskSubjectDto })
+  @IsDefined()
   @IsObject()
+  @ValidateNested()
+  @Type(() => RiskSubjectDto)
   riskSubject: RiskSubjectDto;
 
   @ApiProperty({ default: [Coverage.theft] })
