88335c4aff2ac58b57c93c4682852ae983458144
Gerard Castell <gerardcastell97@gmail.com>
2023-06-04 18:37:13 +0200

    fix: axios import

--- apps/client-web/pages/insurance-contract.tsx
+++ apps/client-web/pages/insurance-contract.tsx
@@ -1,18 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
-import { axios } from '../lib/axios';
 import InsuranceContractForm from '../features/insurance-contract';
-import { Grid, Avatar, Box } from '@mui/material';
+import { Grid } from '@mui/material';
 const InsuranceContract = () => {
-  const { isLoading, isError, data } = useQuery(
-    ['health'],
-    async () => await axios.get('health')
-  );
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (isError) {
-    return <div>Error...</div>;
-  }
   return (
     <Grid
       sx={{ height: 'calc(100vh - 56px)' }}
