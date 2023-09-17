ca38233c60005d50911b4035c254c1555607d03f
Gerard Castell <gerardcastell97@gmail.com>
2023-09-17 16:28:12 +0200

    feat: add config to deploy factory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,11 @@
-import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
+  etherscan: {
+    apiKey: 'QZGWMJ6GWYH9Y8FCMRAZDI9K9EKB1GK64A',
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,18 +4,18 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther('0.001');
+  const lockedAmount = ethers.parseEther('1');
 
-  const lock = await ethers.deployContract('Lock', [unlockTime], {
+  const factory = await ethers.deployContract('Factory', [], {
     value: lockedAmount,
   });
 
-  await lock.waitForDeployment();
+  await factory.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
+    `Factory created with ${ethers.formatEther(
       lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${factory.target}`
   );
 }
 
